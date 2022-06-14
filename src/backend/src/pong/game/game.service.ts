0f7b58d3fbf3c27c24a402092fdb7d4b9d6022b2
RolandGamos <lucasweglarz9@gmail.com>
2022-06-14 18:36:01 +0200

    The server can handle client connection and requests to create rooms and join them #6 #8

--- src/backend/src/pong/game/game.service.ts
+++ src/backend/src/pong/game/game.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
 
 @Injectable()
-export class GameService {}
+export class GameService {
+    createGameRoom(client: Socket): number{
+        var newRoomId = ( Math.random() * 100000 ) | 0;
+
+        client.emit('newGameCreated', {roomId: newRoomId, mySocketId: client.id});
+
+        client.join(newRoomId.toString());
+        return newRoomId;
+    }
+
+    removeGameRoom(roomId: number, rooms: number[]){
+        rooms.splice(rooms.findIndex(element => element == roomId));
+    }
+
+}
