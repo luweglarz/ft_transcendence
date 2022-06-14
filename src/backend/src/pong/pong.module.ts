d9bc46e4d3a57ff41b31ca53cd973045414c1eb1
RolandGamos <lucasweglarz9@gmail.com>
2022-06-14 18:36:01 +0200

    The server can handle client connection and requests to create rooms and join them #6 #8

--- src/backend/src/pong/pong.module.ts
+++ src/backend/src/pong/pong.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { GameController } from './game/game.controller';
+import { GameGateway } from './game/game.gateway';
 import { GameService } from './game/game.service';
 
 @Module({
-  controllers: [GameController],
-  providers: [GameService]
+  controllers: [],
+  providers: [GameGateway, GameService]
 })
 export class PongModule {}
