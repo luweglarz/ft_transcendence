de6ea391599a81eafd4de241b79ffbbe30cada86
RolandGamos <lucasweglarz9@gmail.com>
2022-06-14 21:13:29 +0200

    Added GameService provider to game.gateway.spec.ts

--- src/backend/src/pong/game/game.gateway.spec.ts
+++ src/backend/src/pong/game/game.gateway.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GameGateway } from './game.gateway';
+import { GameService } from './game.service';
 
 describe('GameGateway', () => {
   let gateway: GameGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GameGateway],
+      providers: [GameGateway, GameService],
     }).compile();
 
     gateway = module.get<GameGateway>(GameGateway);
