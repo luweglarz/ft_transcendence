cf212393a1a6a3254c83401097303a6c32981adb
ugtheven <ugotheveny@student.42.fr>
2022-06-15 15:53:33 +0200

    Implementing routing, structure of login page and register page

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }, {
+    path: 'login',
+    component: LoginComponent
+  }, {
+    path: 'register',
+    component: RegisterComponent
+  }, {
+    path: 'not-found',
+    component: NotFoundComponent
+  }, {
+    path: '**',
+    redirectTo: '/not-found'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
