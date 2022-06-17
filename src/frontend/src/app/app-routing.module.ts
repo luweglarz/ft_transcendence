e30401f100b4095dc899b0b0a0d77a1afa10485a
ugtheven <ugotheveny@student.42.fr>
2022-06-17 18:20:09 +0200

    prettier html css + upload on avatar

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { PongComponent } from './pong/pong.component';
+import { PongComponent } from './pages/pong/pong.component';
 
 const routes: Routes = [
   {
