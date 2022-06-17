fa5e63d28ec1755a766fe699e7ca0261f8c75581
mjacq <mjacq@redoute.fr>
2022-06-17 09:47:19 +0200

    auth module templates #4

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { PongModule } from './pong/pong.module';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 import { DbClientModule } from './db-client/db-client.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DbClientModule, PongModule],
+  imports: [DbClientModule, PongModule, AuthModule],
   // controllers: [AppController], // TODO: should probably remove those
   // providers: [AppService],
 })
