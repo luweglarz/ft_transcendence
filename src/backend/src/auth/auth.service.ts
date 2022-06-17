8f1deea5e8468d809946a3608b416bb3b15772b7
mjacq <mjacq@redoute.fr>
2022-06-17 10:35:49 +0200

    signup/in/out routes #4

--- src/backend/src/auth/auth.service.ts
+++ src/backend/src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  signup() {
+    return { message: 'Successfully signed up!' };
+  }
+  signin() {
+    return { message: 'Successfully signed in!' };
+  }
+  signout() {
+    return { message: 'Successfully signed out!' };
+  }
+}
