cdeb080d5a5be52708d4a69455285b114a2ef974
mjacq <mjacq@redoute.fr>
2022-06-17 16:52:41 +0200

    authDto: more fields (email, password, passwordConfirm) #4

--- src/backend/src/auth/dto/auth.dto.ts
+++ src/backend/src/auth/dto/auth.dto.ts
@@ -1,7 +1,34 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class AuthDto {
   @IsString()
   @IsNotEmpty()
+  @MaxLength(42)
   username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(42)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  password: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(42)
+  passwordConfirm: string;
 }
+
+// ref for the password checking: https://stackoverflow.com/questions/60451337/password-confirmation-in-typescript-with-class-validator
