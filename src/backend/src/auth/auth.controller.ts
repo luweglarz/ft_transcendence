86891d647214a7a66bb56097c8f523f0ca847e61
mjacq <mjacq@redoute.fr>
2022-06-17 16:36:43 +0200

    whitelist pipes

--- src/backend/src/auth/auth.controller.ts
+++ src/backend/src/auth/auth.controller.ts
@@ -9,10 +9,7 @@ export class AuthController {
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
   signup(@Body() dto: AuthDto) {
-    console.log({
-      username: dto.username,
-      typeofUsername: typeof dto.username,
-    });
+    console.log(dto);
     return this.authService.signup(dto);
   }
 
