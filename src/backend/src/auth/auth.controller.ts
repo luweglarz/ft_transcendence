a993b2c6c53914c07b3baa2775a5e7d70a6e2047
mjacq <mjacq@redoute.fr>
2022-06-17 10:35:49 +0200

    signup/in/out routes #4

--- src/backend/src/auth/auth.controller.ts
+++ src/backend/src/auth/auth.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  signup() {
+    return this.authService.signup();
+  }
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  signin() {
+    return this.authService.signin();
+  }
+
+  @Post('signout')
+  @HttpCode(HttpStatus.OK)
+  signout() {
+    return this.authService.signout();
+  }
+}
