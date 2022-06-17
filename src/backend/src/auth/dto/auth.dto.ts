129ebef6c23bf28b5dd4b703ad1ab24d5c293085
mjacq <mjacq@redoute.fr>
2022-06-17 16:25:48 +0200

    fix controller test dependency issue

--- src/backend/src/auth/dto/auth.dto.ts
+++ src/backend/src/auth/dto/auth.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class AuthDto {
   @IsString()
+  @IsNotEmpty()
   username: string;
 }
