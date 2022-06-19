4843f243353074dde1467f7ef2a8a32f5bee10c7
ugtheven <ugotheveny@student.42.fr>
2022-06-19 03:22:11 +0200

    revoked old leaked secret and replaced with new one

--- src/frontend/src/app/pages/login/login.component.ts
+++ src/frontend/src/app/pages/login/login.component.ts
@@ -43,9 +43,6 @@ export class LoginComponent implements OnInit {
           console.log('Error');
         }
       });
-
-    //Redirect to the 42 sign-in page
-    this.document.location.href = "https://api.intra.42.fr/oauth/authorize?client_id=eeb0e0a43e664699fe90ad4eefdec48dc656e8b75737d363731f5221dd57f6dd&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2F&response_type=code";
   }
 
 }
