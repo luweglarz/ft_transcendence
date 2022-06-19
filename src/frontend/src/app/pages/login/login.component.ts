59114029767cdd5842dd601d55fc275c158543e6
ugtheven <ugotheveny@student.42.fr>
2022-06-19 03:13:45 +0200

    hiding the secret of API in git-secret

--- src/frontend/src/app/pages/login/login.component.ts
+++ src/frontend/src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { DOCUMENT } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -30,15 +31,21 @@ export class LoginComponent implements OnInit {
     };
     //Body
     const body =
-      'grant_type=client_credentials&client_id=eeb0e0a43e664699fe90ad4eefdec48dc656e8b75737d363731f5221dd57f6dd&client_secret=***REMOVED***';
+      'grant_type=client_credentials&client_id=' + environment.apiUid + '&' + 'client_secret=' + environment.apiSecret;
     //POST
     this.http
-      .post<any>('https://api.intra.42.fr/oauth/token', body, { headers })
-      .subscribe((data) => {
-        console.log(data);
+      .post<any>('https://api.intra.42.fr/oauth/token', body, { headers }).subscribe({
+        next: data => {
+          this.access_token = data.access_token;
+          console.log('Recuperation de ' + data.access_token);
+        },
+        error: error => {
+          console.log('Error');
+        }
       });
 
     //Redirect to the 42 sign-in page
-    //this.document.location.href = "https://api.intra.42.fr/oauth/authorize?client_id=eeb0e0a43e664699fe90ad4eefdec48dc656e8b75737d363731f5221dd57f6dd&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2F&response_type=code";
+    this.document.location.href = "https://api.intra.42.fr/oauth/authorize?client_id=eeb0e0a43e664699fe90ad4eefdec48dc656e8b75737d363731f5221dd57f6dd&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2F&response_type=code";
   }
+
 }
