2c5a5fe46879041b3ecc6c3add423076c9020682
jchemoun <jchemoun@student.42.fr>
2022-06-21 12:07:34 +0200

    post merge

--- src/backend/src/chat/chat.module.ts
+++ src/backend/src/chat/chat.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ChatGateway } from './chat.gateway';
+import { ChatController } from './chat.controller';
 
-@Module({})
+@Module({
+  providers: [ChatGateway],
+  controllers: [ChatController]
+})
 export class ChatModule {}
