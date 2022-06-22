1b882987bb8cc66a5486424e092aab8a96c1e738
mjacq <mjacq@redoute.fr>
2022-06-22 17:12:09 +0200

    sigin/signup: dedicated dtos

--- src/backend/src/auth/auth.controller.ts
+++ src/backend/src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthDto } from './dto';
+import { UsernameSigninDto, EmailSignupDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
@@ -8,21 +8,21 @@ export class AuthController {
 
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
-  signup(@Body() dto: AuthDto) {
+  signup(@Body() dto: EmailSignupDto) {
     console.log(dto);
     return this.authService.signup(dto);
   }
 
   @Post('signin')
   @HttpCode(HttpStatus.OK)
-  async signin(@Body() dto: AuthDto) {
+  async signin(@Body() dto: UsernameSigninDto) {
     // console.log(`OAUTH client ID: ${process.env['OAUTH_42_CLIENT_ID']}`);
     return this.authService.signin(dto);
   }
 
   @Post('signout')
   @HttpCode(HttpStatus.OK)
-  signout(@Body() dto: AuthDto) {
+  signout(@Body() dto: EmailSignupDto) {
     return this.authService.signout(dto);
   }
 }
