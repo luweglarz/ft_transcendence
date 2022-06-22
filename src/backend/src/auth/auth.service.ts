55f9e6efd7217d6ef840a45ee3ea7f2b1172c685
mjacq <mjacq@redoute.fr>
2022-06-22 17:24:23 +0200

    signup/signin: logging

--- src/backend/src/auth/auth.service.ts
+++ src/backend/src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, Logger } from '@nestjs/common';
 import * as argon from 'argon2';
 import { DbService } from 'src/db/db.service';
 import { UsernameSigninDto, EmailSignupDto } from './dto';
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
   constructor(private db: DbService) {}
 
   async signup(dto: EmailSignupDto) {
@@ -16,7 +17,8 @@ export class AuthService {
         password: pwdHash,
       },
     });
-    return { message: `${dto.username} successfully signed up! ${pwdHash}` };
+    this.logger.log(`New user '${dto.username}' successfully signed up!`);
+    return { message: `${dto.username} successfully signed up!` };
   }
 
   async signin(dto: UsernameSigninDto) {
@@ -24,7 +26,8 @@ export class AuthService {
       where: { username: dto.username },
     });
     if (user && (await argon.verify(user.password, dto.password))) {
-      return { message: `${user.username} Successfully signed in!` };
+      this.logger.log(`User '${dto.username}' successfully signed in!`);
+      return { message: `${user.username} successfully signed in!` };
     } else throw new ForbiddenException({ message: 'Credentials incorrect' });
   }
 
