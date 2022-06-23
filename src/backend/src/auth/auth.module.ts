fff3473cd430d38bdf2df99ac16bfefe5f42e61a
mjacq <mjacq@redoute.fr>
2022-06-23 16:37:36 +0200

    add guard to check the incoming request's jwt

--- src/backend/src/auth/auth.module.ts
+++ src/backend/src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategy';
 
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
