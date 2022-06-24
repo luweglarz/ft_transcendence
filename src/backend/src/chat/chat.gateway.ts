3c4ba12d8c7c9ccee4479282fbf9d477e6a231b1
jchemoun <jchemoun@student.42.fr>
2022-06-24 16:39:34 +0200

    added room-user service

--- src/backend/src/chat/chat.gateway.ts
+++ src/backend/src/chat/chat.gateway.ts
@@ -1,7 +1,27 @@
 import { SubscribeMessage, WebSocketGateway, OnGatewayConnection, OnGatewayDisconnect, WebSocketServer } from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+import { RoomUserService } from './room-user/room-user.service';
+import { RoomService } from './room/room.service';
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer()
+  server: Server;
+
+  constructor(
+    private roomService: RoomService,
+    private roomUserService: RoomUserService
+  ) {}
+  
+ async handleConnection(socket: Socket) {
+   
+  }
+
+  async handleDisconnect(socket: Socket) {
+    
+  }
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
--- src/backend/src/chat/chat.module.ts
+++ src/backend/src/chat/chat.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { ChatController } from './chat.controller';
 import { RoomService } from './room/room.service';
+import { RoomUserService } from './room-user/room-user.service';
 
 @Module({
-  providers: [ChatGateway, RoomService],
+  providers: [ChatGateway, RoomService, RoomUserService],
   controllers: [ChatController]
 })
 export class ChatModule {}
