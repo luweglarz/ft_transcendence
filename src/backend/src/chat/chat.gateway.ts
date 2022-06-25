2451ebae0e52363e0e69e7442b0976ceb32547e1
jchemoun <jchemoun@student.42.fr>
2022-06-25 10:07:15 +0200

    added message service && progress on prisma

--- src/backend/src/chat/chat.gateway.ts
+++ src/backend/src/chat/chat.gateway.ts
@@ -1,7 +1,9 @@
-import { SubscribeMessage, WebSocketGateway, OnGatewayConnection, OnGatewayDisconnect, WebSocketServer } from '@nestjs/websockets';
+import { SubscribeMessage, WebSocketGateway, OnGatewayConnection, OnGatewayDisconnect, WebSocketServer, MessageBody } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
 import { RoomUserService } from './room-user/room-user.service';
 import { RoomService } from './room/room.service';
+import { MessageService } from './message/message.service';
+import { Room, User } from '@prisma/client';
 
 @WebSocketGateway()
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -10,8 +12,9 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   server: Server;
 
   constructor(
-    private roomService: RoomService,
-    private roomUserService: RoomUserService
+    private roomService:      RoomService,
+    private roomUserService:  RoomUserService,
+    private messageService:   MessageService
   ) {}
   
  async handleConnection(socket: Socket) {
@@ -20,6 +23,11 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   async handleDisconnect(socket: Socket) {
     
+  }
+
+  @SubscribeMessage('createMessage')
+  async create(@MessageBody() content: string, room: Room, user: User) {
+    this.messageService.createMessage({content, room: {connect: room}, user: {connect: user}})
   }
 
   @SubscribeMessage('message')
--- src/backend/src/chat/chat.module.ts
+++ src/backend/src/chat/chat.module.ts
@@ -3,9 +3,10 @@ import { ChatGateway } from './chat.gateway';
 import { ChatController } from './chat.controller';
 import { RoomService } from './room/room.service';
 import { RoomUserService } from './room-user/room-user.service';
+import { MessageService } from './message/message.service';
 
 @Module({
-  providers: [ChatGateway, RoomService, RoomUserService],
+  providers: [ChatGateway, RoomService, RoomUserService, MessageService],
   controllers: [ChatController]
 })
 export class ChatModule {}
