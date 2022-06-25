ed1d63bde8264fc4b7b252491752edab17a76980
jchemoun <jchemoun@student.42.fr>
2022-06-25 12:46:40 +0200

    progress on gateway && prisma

--- src/backend/src/chat/chat.gateway.ts
+++ src/backend/src/chat/chat.gateway.ts
@@ -3,7 +3,7 @@ import { Socket, Server } from 'socket.io';
 import { RoomUserService } from './room-user/room-user.service';
 import { RoomService } from './room/room.service';
 import { MessageService } from './message/message.service';
-import { Room, User } from '@prisma/client';
+import { Room, RoomType, User, RoomUser } from '@prisma/client';
 
 @WebSocketGateway()
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -14,20 +14,30 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(
     private roomService:      RoomService,
     private roomUserService:  RoomUserService,
-    private messageService:   MessageService
+    private messageService:   MessageService,
+    //private connectedUsers:   {[userId: number]: Socket}
   ) {}
   
  async handleConnection(socket: Socket) {
-   
+    
   }
 
   async handleDisconnect(socket: Socket) {
     
   }
 
+  @SubscribeMessage('createRoom')
+  async createRoom(@MessageBody() name: string, roomType: RoomType, owner: User, password?: string) {
+    if (roomType === 'PROTECTED' && !password)
+      return 'Error';
+    this.roomService.createRoom({ name, password, roomType, users: {create: {user: {connect: owner}, role: 'OWNER'}} });
+    // needs emit once i can find how to identify user by connection to server
+  }
+
   @SubscribeMessage('createMessage')
-  async create(@MessageBody() content: string, room: Room, user: User) {
-    this.messageService.createMessage({content, room: {connect: room}, user: {connect: user}})
+  async createMessage(@MessageBody() content: string, room: Room, user: User) {
+    this.messageService.createMessage({ content, room: {connect: room}, user: {connect: user} });
+    // needs emit once i can find how to identify user by connection to server
   }
 
   @SubscribeMessage('message')
