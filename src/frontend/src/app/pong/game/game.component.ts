2bdd3a2c09f153f143199b59da078ddff37aed8c
RolandGamos <lucasweglarz9@gmail.com>
2022-06-29 15:59:25 +0200

    Changed the game map #27

--- src/frontend/src/app/pong/game/game.component.ts
+++ src/frontend/src/app/pong/game/game.component.ts
@@ -31,14 +31,23 @@ export class GameComponent implements OnInit {
     //Get an object to draw on the canvas
     this.context = this.gameCanvas.nativeElement.getContext('2d');
     this.socket.on('racketPosition', (pos1: any, pos2: any) => {
+      //Should try to clear only the rackets and not the whole canvas ->
       this.context.clearRect(
         0,
         0,
         this.gameCanvas.nativeElement.width,
         this.gameCanvas.nativeElement.height,
       );
+      //To avoid redrawing the whole map everytime there is position change
       this.context.fillRect(pos1.x, pos1.y, 35, 110);
       this.context.fillRect(pos2.x, pos2.y, 35, 110);
+      this.context.strokeStyle = "black";
+      this.context.lineWidth = 5;
+      this.context.strokeRect(10,10, this.gameCanvas.nativeElement.width - 20, this.gameCanvas.nativeElement.height - 20);
+      this.context.moveTo(425, 515)
+      this.context.lineTo(425, 10);
+      this.context.strokeStyle = "black";
+      this.context.stroke();
     });
   }
 
