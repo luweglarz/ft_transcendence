208f30e113ba0d54cc0fd760cdc7b21a6c9fbb8a
mjacq <mjacq@redoute.fr>
2022-06-29 18:59:15 +0200

    Use an enum for prisma error codes (cleaner code)

--- src/backend/src/auth/strategy/jwt.strategy.ts
+++ src/backend/src/auth/strategy/jwt.strategy.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
-    super({
+    super(<StrategyOptions>{
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env['JWT_SECRET'],
     });
