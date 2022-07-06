f1c6ff2641b6c06811392824d2104d588b338f5c
RolandGamos <lucasweglarz9@gmail.com>
2022-07-06 16:31:30 +0200

    Smoother paddle movement #27 #6

--- src/backend/src/pong/game/game.service.ts
+++ src/backend/src/pong/game/game.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Socket } from 'socket.io';
+import { Socket, Server } from 'socket.io';
 import { Player } from '../class/player';
 import { Room } from '../class/room';
 
@@ -24,5 +24,42 @@ export class GameService {
   findPlayer(room: Room, client: Socket): Player {
     if (room.players[0].socket === client) return room.players[0];
     else if (room.players[1].socket === client) return room.players[1];
+  }
+
+  checkBorderCollision(playerInfos: Player): boolean {
+    if (
+      playerInfos.velocity == -1 &&
+      playerInfos.y <= playerInfos.borderCollisionUp
+    ) {
+      playerInfos.y = playerInfos.borderCollisionUp;
+      return true;
+    } else if (
+      playerInfos.velocity == 1 &&
+      playerInfos.y >= playerInfos.borderCollisionDown
+    ) {
+      playerInfos.y = playerInfos.borderCollisionDown;
+      return true;
+    }
+    return false;
+  }
+
+  gameLoop(players: Player[], gameRoom: Room, server: Server) {
+    setInterval(() => {
+      if (this.checkBorderCollision(players[0])) {
+        players[0].velocity = 0;
+      }
+      if (this.checkBorderCollision(players[1])) {
+        players[1].velocity = 0;
+      }
+      players[0].y += players[0].velocity * players[0].speed;
+      players[1].y += players[1].velocity * players[1].speed;
+      server
+        .to(gameRoom.uuid)
+        .emit(
+          'gameUpdate',
+          { x: players[0].x, y: players[0].y },
+          { x: players[1].x, y: players[1].y },
+        );
+    }, 5);
   }
 }
