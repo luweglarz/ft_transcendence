649b391ee025401d120a4f6f878bd1e1737c5cbb
mjacq <mjacq@redoute.fr>
2022-07-08 14:27:57 +0200

    OAuth2Guard: fetch info from 42's api

--- src/backend/src/auth/strategy/oauth2.strategy.ts
+++ src/backend/src/auth/strategy/oauth2.strategy.ts
@@ -1,13 +1,15 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, StrategyOptions, VerifyCallback } from 'passport-oauth2';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class OAuth2Strategy extends PassportStrategy(Strategy, 'oauth2') {
   private readonly logger = new Logger(OAuth2Strategy.name);
 
   // @doc http://www.passportjs.org/packages/passport-oauth2/
-  constructor() {
+  constructor(private readonly httpService: HttpService) {
     super(<StrategyOptions>{
       authorizationURL: 'https://api.intra.42.fr/oauth/authorize',
       tokenURL: 'https://api.intra.42.fr/oauth/token',
@@ -17,16 +19,24 @@ export class OAuth2Strategy extends PassportStrategy(Strategy, 'oauth2') {
     });
   }
 
-  validate(
+  async validate(
     accessToken: string,
     refreshToken: string,
-    profile: any,
+    profile: any, // empty object
     verified: VerifyCallback,
   ) {
     this.logger.debug(`${OAuth2Strategy.name}.${this.validate.name} called`);
     this.logger.debug(`accessToken: ${accessToken}`);
     this.logger.debug(`refreshToken: ${refreshToken}`);
-    this.logger.debug(`profile: ${JSON.stringify(profile, null, 2)}`); // empty
-    verified(null, profile);
+    const userObservable = this.httpService.get(
+      'https://api.intra.42.fr/v2/me',
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      },
+    );
+    const { data } = await lastValueFrom(userObservable); // lastValueFrom: convert observable to promise
+    // this.logger.debug(`data: ${JSON.stringify(data, null, 2)}`); // user format: https://api.intra.42.fr/apidoc/2.0/users/me.html
+    const user = { email: data.email, login: data.login };
+    verified(null, user, { accessToken, refreshToken, profile });
   }
 }
