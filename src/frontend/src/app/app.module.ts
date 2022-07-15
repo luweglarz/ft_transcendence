5b54267c57b63c2f96666c0b6c24ef2bd3b11cf8
mjacq <mjacq@redoute.fr>
2022-07-15 16:05:04 +0200

    cleaner imports

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { PongModule } from './pong/pong.module';
+import { AuthModule } from './auth';
+import { AppComponent } from './app.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { HomeComponent } from './pages/home/home.component';
-import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProfilComponent } from './components/profil/profil.component';
 import { LadderComponent } from './components/ladder/ladder.component';
 import { SocialComponent } from './components/social/social.component';
 import { ChatbarComponent } from './components/chatbar/chatbar.component';
-import { PongModule } from './pong/pong.module';
-import { RouterModule } from '@angular/router';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
