9654830a3c28aeef20f306aca438a38a4ddfab0d
mjacq <mjacq@redoute.fr>
2022-07-16 16:46:40 +0200

    oauth from the frontend (logic is there but refactor is needed)

--- src/frontend/src/app/auth/register/register.component.ts
+++ src/frontend/src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -16,12 +18,31 @@ export class RegisterComponent implements OnInit {
     email: new FormControl(null, Validators.required),
   });
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router,
+  ) {
     //
   }
 
   ngOnInit(): void {
-    //
+    this.route.queryParams.subscribe((params) => {
+      console.log(params);
+      if (params['code'])
+        this.http
+          .get(`http://localhost:3000/oauth/redirect?code=${params['code']}`)
+          .subscribe((response: any) => {
+            console.log(response);
+            localStorage.setItem('jwt', response['jwt']);
+            this.router.navigate(['/register']);
+          });
+      else {
+        const jwt = localStorage.getItem('jwt');
+        if (jwt) console.log(`My jwt: ${jwt}`);
+      }
+    });
   }
 
   choose2FA() {
