89a5a83c5996ea301fef72b6dba5fbb55fb07e74
mjacq <mjacq@redoute.fr>
2022-07-18 14:24:34 +0200

    Change signup with 42 button

--- src/frontend/src/app/auth/signup/signup.component.ts
+++ src/frontend/src/app/auth/signup/signup.component.ts
@@ -39,4 +39,9 @@ export class SignUpComponent implements OnInit {
       .post('http://localhost:3000/auth/local/signup', this.registerForm.value)
       .subscribe((response) => console.log(response));
   }
+
+  oAuth() {
+    window.location.href =
+      'https://api.intra.42.fr/oauth/authorize?client_id=86fb252b97a66621fd8e06b39794ec809a80cef7383535c464d0310c4ca7418a&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fauth%2Foauth42%2Fcallback&response_type=code';
+  }
 }
