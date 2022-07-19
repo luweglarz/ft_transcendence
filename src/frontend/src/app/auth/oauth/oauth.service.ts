d2a83c72f8d6aad25a90ebb1467d9a617b365a8d
mjacq <mjacq@redoute.fr>
2022-07-19 18:19:49 +0200

    cleanup component and service

--- src/frontend/src/app/auth/oauth/oauth.service.ts
+++ src/frontend/src/app/auth/oauth/oauth.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtService } from '../jwt';
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +13,45 @@ export class OAuthService {
   readonly redirect_uri = encodeURI(
     'http://localhost:4200/auth/oauth42/callback',
   );
+  readonly backend_temp_token_url =
+    'http://localhost:3000/auth/oauth42/signup-temp-token';
+  readonly backend_signin_url = 'http://localhost:3000/auth/oauth42/signin';
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private jwt: JwtService,
+  ) {}
 
   authorize(state: 'signin' | 'signup') {
     window.location.href = `${this.authorize_url}?client_id=${this.client_id}&redirect_uri=${this.redirect_uri}&response_type=code&state=${state}`;
+  }
+
+  getSignupTempToken(code: string) {
+    this.http
+      .get<{ jwt: string }>(`${this.backend_temp_token_url}?code=${code}`)
+      .subscribe({
+        next: (response) => {
+          this.router.navigate(['/auth/signup'], {
+            queryParams: { type: 'oauth', jwt: response.jwt },
+            replaceUrl: true, // prevent going back to the callback page
+          });
+        },
+        error: (err) => console.error(err),
+      });
+  }
+
+  getSignInToken(code: string) {
+    this.http
+      .get<{ jwt: string }>(`${this.backend_signin_url}?code=${code}`)
+      .subscribe({
+        next: (response) => {
+          this.jwt.setToken(response.jwt);
+          this.router.navigate(['/'], {
+            replaceUrl: true,
+          });
+        },
+        error: (err) => console.error(err),
+      });
   }
 }
