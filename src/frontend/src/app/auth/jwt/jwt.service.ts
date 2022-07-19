fe99fadf57e7939bed2a48644eb2f4b72e38b940
mjacq <mjacq@redoute.fr>
2022-07-19 15:59:08 +0200

    component to print user auth info

--- src/frontend/src/app/auth/jwt/jwt.service.ts
+++ src/frontend/src/app/auth/jwt/jwt.service.ts
@@ -1,5 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import jwtDecode from 'jwt-decode';
+
+export interface JwtPayload {
+  sub: number;
+  username: string;
+  iat: number;
+  exp: number;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -21,6 +29,24 @@ export class JwtService {
   clearToken() {
     localStorage.removeItem('jwt');
     this._token = null;
+  }
+
+  getPayload() {
+    const token = this.getToken();
+    if (token) return jwtDecode<JwtPayload>(token);
+    else return undefined;
+  }
+
+  isValid() {
+    const payload = this.getPayload();
+    if (payload && payload.exp * 1000 > Date.now()) return true;
+    else return false;
+  }
+
+  expirationString() {
+    const payload = this.getPayload();
+    if (payload) return new Date(payload.exp * 1000).toString();
+    else return '';
   }
 
   testToken() {
