6c70cf9149f68a1057f154718777192a363fc28a
mjacq <mjacq@redoute.fr>
2022-07-19 18:19:49 +0200

    cleanup component and service

--- src/frontend/src/app/auth/oauth/oauth.component.ts
+++ src/frontend/src/app/auth/oauth/oauth.component.ts
@@ -1,7 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { JwtService } from '../jwt';
+import { ActivatedRoute } from '@angular/router';
+import { OAuthService } from './oauth.service';
 
 @Component({
   selector: 'app-oauth',
@@ -9,42 +8,17 @@ import { JwtService } from '../jwt';
   styleUrls: ['./oauth.component.css'],
 })
 export class OauthComponent implements OnInit {
-  constructor(
-    private route: ActivatedRoute,
-    private http: HttpClient,
-    private router: Router,
-    private jwt: JwtService,
-  ) {}
+  constructor(private route: ActivatedRoute, private service: OAuthService) {}
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params) => {
       if (params['code']) {
-        if (params['state'] == 'signup') {
-          this.http
-            .get(
-              `http://localhost:3000/auth/oauth42/signup-temp-token?code=${params['code']}`,
-            )
-            .subscribe((response: any) => {
-              // this.jwt.setToken(response['jwt']);
-              this.router.navigate(['/auth/signup'], {
-                queryParams: { type: 'oauth', jwt: response['jwt'] },
-                replaceUrl: true, // cannot go back to the callback page
-              });
-            });
-        } else if (params['state'] == 'signin') {
-          this.http
-            .get(
-              `http://localhost:3000/auth/oauth42/signin?code=${params['code']}`,
-            )
-            .subscribe((response: any) => {
-              this.jwt.setToken(response['jwt']);
-              this.router.navigate(['/'], {
-                replaceUrl: true,
-              });
-            });
-        }
+        if (params['state'] == 'signup')
+          this.service.getSignupTempToken(params['code']);
+        else if (params['state'] == 'signin')
+          this.service.getSignInToken(params['code']);
       } else {
-        if (this.jwt.getToken()) console.log(`My jwt: ${this.jwt.getToken()}`);
+        console.warn('"code" or "state" query param is missing.');
       }
     });
   }
