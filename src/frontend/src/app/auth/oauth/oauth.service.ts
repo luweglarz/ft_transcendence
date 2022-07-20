31e38fa22efe41cbe646f372040decd8c4db870c
mjacq <mjacq@redoute.fr>
2022-07-20 13:55:03 +0200

    let the frontend ask the backend fork public oauth client_id

--- src/frontend/src/app/auth/oauth/oauth.service.ts
+++ src/frontend/src/app/auth/oauth/oauth.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,14 +6,22 @@ import { Injectable } from '@angular/core';
 })
 export class OAuthService {
   private readonly authorize_url = 'https://api.intra.42.fr/oauth/authorize';
-  private readonly client_id =
-    '86fb252b97a66621fd8e06b39794ec809a80cef7383535c464d0310c4ca7418a';
+  private readonly get_client_id_url =
+    'http://localhost:3000/auth/oauth42/client_id';
+  private client_id = '';
   private readonly frontend_url = 'http://localhost:4200';
   private readonly redirect_uri = encodeURI(
     `${this.frontend_url}/auth/oauth42/callback`,
   );
 
+  constructor(private http: HttpClient) {}
+
   authorize(state: 'signin' | 'signup') {
-    window.location.href = `${this.authorize_url}?client_id=${this.client_id}&redirect_uri=${this.redirect_uri}&response_type=code&state=${state}`;
+    this.http
+      .get<{ client_id: string }>(this.get_client_id_url)
+      .subscribe((response) => {
+        this.client_id = response.client_id;
+        window.location.href = `${this.authorize_url}?client_id=${this.client_id}&redirect_uri=${this.redirect_uri}&response_type=code&state=${state}`;
+      });
   }
 }
