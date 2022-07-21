ba9cfe4cd7e2dc0496ef6c050cd64070f4c5a3cb
mjacq <mjacq@redoute.fr>
2022-07-21 18:59:41 +0200

    avatar-upload: display currently loaded image

--- src/frontend/src/app/avatar/avatar-upload/avatar-upload.component.ts
+++ src/frontend/src/app/avatar/avatar-upload/avatar-upload.component.ts
@@ -11,4 +11,18 @@ export class AvatarUploadComponent implements OnInit {
   ngOnInit(): void {
     //
   }
+
+  processUpload(upload: HTMLInputElement) {
+    if (upload.files) {
+      const image = upload.files[0];
+      console.log(`Loaded image: ${image.name}`);
+      const reader = new FileReader();
+      reader.addEventListener('load', (ev: ProgressEvent<FileReader>) => {
+        // The load event is fired when a file has been read successfully.
+        if (typeof ev.target?.result === 'string')
+          this.image_url = ev.target?.result;
+      });
+      reader.readAsDataURL(image);
+    }
+  }
 }
