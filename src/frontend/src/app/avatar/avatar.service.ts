9b3794817d8171df4afd861d5b0f7d5b67d0f242
mjacq <mjacq@redoute.fr>
2022-07-22 17:30:32 +0200

    avatar-upload: refact

--- src/frontend/src/app/avatar/avatar.service.ts
+++ src/frontend/src/app/avatar/avatar.service.ts
@@ -1,19 +1,55 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Avatar } from './interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AvatarService {
-  image_file?: File;
+  readonly default_src = '/assets/images/default-avatar.png';
+  private avatar: Avatar = {
+    src: this.default_src,
+    file: undefined,
+  };
 
   constructor(private http: HttpClient) {}
 
+  get src() {
+    return this.avatar.src;
+  }
+
+  get file() {
+    return this.avatar.file;
+  }
+
+  update(data: Partial<Avatar>) {
+    if (data.src) {
+      this.avatar.src = data.src;
+      if (!data.file) {
+        // TODO
+        console.info('Avatar: url to file not implemented yet');
+      }
+    }
+    if (data.file) {
+      this.avatar.file = data.file;
+      if (!data.src) {
+        const reader = new FileReader();
+        reader.addEventListener('load', (ev: ProgressEvent<FileReader>) => {
+          // The load event is fired when a file has been read successfully.
+          if (typeof ev.target?.result === 'string') {
+            this.avatar.src = ev.target.result;
+          }
+        });
+        reader.readAsDataURL(data.file);
+      }
+    }
+  }
+
   backendUpload() {
-    if (this.image_file) {
+    if (this.avatar.file) {
       const formData = new FormData();
-      formData.append('avatar', this.image_file);
+      formData.append('avatar', this.avatar.file);
       this.http
         .post(`${environment.backend}/auth/upload/avatar`, formData)
         .subscribe();
