36d8f67b130cfadeb2cd80ea97b240c7c4657c9b
mjacq <mjacq@redoute.fr>
2022-07-22 17:30:32 +0200

    avatar-upload: refact

--- src/frontend/src/app/avatar/avatar-upload/avatar-upload.component.ts
+++ src/frontend/src/app/avatar/avatar-upload/avatar-upload.component.ts
@@ -7,29 +7,20 @@ import { AvatarService } from '../avatar.service';
   styleUrls: ['./avatar-upload.component.css'],
 })
 export class AvatarUploadComponent implements OnInit {
-  @Input() image_url = '/assets/images/default-avatar.png';
-  image_file?: File;
+  @Input() default_src = this.avatar.default_src;
 
-  constructor(private service: AvatarService) {}
+  constructor(public readonly avatar: AvatarService) {}
 
   ngOnInit(): void {
-    //
+    if (this.default_src != this.avatar.default_src)
+      this.avatar.update({ src: this.default_src });
   }
 
   processUpload(upload: HTMLInputElement) {
     if (upload.files) {
       const image = upload.files[0];
       console.log(`Loaded image: ${image.name}`);
-      const reader = new FileReader();
-      reader.addEventListener('load', (ev: ProgressEvent<FileReader>) => {
-        // The load event is fired when a file has been read successfully.
-        if (typeof ev.target?.result === 'string') {
-          this.image_url = ev.target?.result;
-          this.image_file = image;
-          this.service.image_file = this.image_file;
-        }
-      });
-      reader.readAsDataURL(image);
+      this.avatar.update({ file: image });
     }
   }
 }
