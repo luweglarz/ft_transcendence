9ab1a0e8d07ca118dc1efc3319f939a5fc09d679
ugtheven <ugotheveny@student.42.fr>
2022-07-26 00:46:59 +0200

    working on profil page #56

--- src/frontend/src/app/home-page/profil/profil.component.ts
+++ src/frontend/src/app/home-page/profil/profil.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profil.component.css'],
 })
 export class ProfilComponent implements OnInit {
+
+  username: string = 'username';
+  rank: number = 1;
+  elo: number = 0;
+  win: number = 0;
+  loss: number = 0;
+  winStreak: number = 0;
+  lossStreak: number = 0;
+
+
   constructor() {
     //
   }
