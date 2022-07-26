fce2f5c2c6e5633c270d052c6a716605b69a4fd6
mjacq <mjacq@redoute.fr>
2022-07-26 17:51:25 +0200

    backend chat: update .spec dependencies

--- src/backend/src/chat/chat.gateway.spec.ts
+++ src/backend/src/chat/chat.gateway.spec.ts
@@ -1,12 +1,24 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { DbService } from 'src/db/db.service';
 import { ChatGateway } from './chat.gateway';
+import { MessageService } from './message/message.service';
+import { RoomUserService } from './room-user/room-user.service';
+import { RoomService } from './room/room.service';
 
 describe('ChatGateway', () => {
   let gateway: ChatGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ChatGateway],
+      providers: [
+        ChatGateway,
+        DbService,
+        RoomUserService,
+        MessageService,
+        JwtService,
+        RoomService,
+      ],
     }).compile();
 
     gateway = module.get<ChatGateway>(ChatGateway);
--- src/backend/src/chat/room/room.service.spec.ts
+++ src/backend/src/chat/room/room.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { DbService } from 'src/db/db.service';
 import { RoomService } from './room.service';
 
 describe('RoomService', () => {
@@ -6,7 +7,7 @@ describe('RoomService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoomService],
+      providers: [RoomService, DbService],
     }).compile();
 
     service = module.get<RoomService>(RoomService);
