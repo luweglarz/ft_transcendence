fca44e427dda8dd8952b1761da51e59cb0d06527
mjacq <mjacq@redoute.fr>
2022-07-28 17:33:23 +0200

    backend auth: small cleanup

--- src/backend/src/auth/auth.service.ts
+++ src/backend/src/auth/auth.service.ts
@@ -128,13 +128,7 @@ export class AuthService {
     if (!data)
       throw new ForbiddenException('Could not fetch user with 42 OAuth2 API');
     const user = new OAuthUserDto();
-    for (const key of [
-      'login',
-      'email',
-      'image_url',
-      // 'first_name',
-      // 'last_name',
-    ]) {
+    for (const key of ['login', 'email', 'image_url']) {
       user[key] = data[key];
     }
     const errors = await validate(user, {
---
+++ src/backend/src/auth/dto/local-auth.dto.ts
@@ -0,0 +1,33 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class LocalSignupDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(42)
+  username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(42)
+  password: string;
+}
+
+export class LocalSigninDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
