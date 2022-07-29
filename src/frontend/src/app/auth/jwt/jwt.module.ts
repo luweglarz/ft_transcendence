98ed5ef5bbaa53c95c45e0930be70fe19286a195
mjacq <mjacq@redoute.fr>
2022-07-29 10:23:16 +0200

    remove useless 'provide: ...' in modules auth and jwt

--- src/frontend/src/app/auth/jwt/jwt.module.ts
+++ src/frontend/src/app/auth/jwt/jwt.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { JwtService } from './jwt.service';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  providers: [JwtService],
 })
 export class JwtModule {}
