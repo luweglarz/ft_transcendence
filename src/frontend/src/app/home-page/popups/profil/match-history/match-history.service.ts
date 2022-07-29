2297a32d2b14b0ca94aec5b783a76db5e800c480
ugtheven <ugotheveny@student.42.fr>
2022-07-29 17:48:40 +0200

    Retrieving the game history #68

--- src/frontend/src/app/home-page/popups/profil/match-history/match-history.service.ts
+++ src/frontend/src/app/home-page/popups/profil/match-history/match-history.service.ts
@@ -1,26 +1,64 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-//import { Observable } from 'rxjs';
+import { ComponentFactoryResolver, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-// interface WinHistory {
-//   id: number;
-//   createdAt: string;
-//   winnerId: number;
-//   winnerGoals: number;
-//   loserId: number;
-//   loserGoals: number;
-// }
+ interface Game {
+   id: number;
+   createdAt: string;
+   winnerId: number;
+   winnerGoals: number;
+   loserId: number;
+   loserGoals: number;
+ }
+
+ interface WinHistory {
+    wins: Array<Game>;
+    nbWins: number;
+ }
+
+ interface LoseHistory {
+  loses: Array<Game>;
+  nbWins: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatchHistoryService {
 
-  //winHistory: Observable<WinHistory[]>;
+  nbWins = 0;
+  nbLoses = 0;
 
-  constructor(private http: HttpClient) { }
+  gameHistory: Array<Game> = [];
+  nbGames = 0;
 
-  getWinHistory() {
-    console.log(this.http.get('/game/wins\?username\=ugtheven'));
+  constructor(private http: HttpClient) {
+  }
+
+  generateHistory() {
+
+  }
+
+  testButton() {
+    //WINS
+    this.http.get<WinHistory>('http://localhost:3000/game/wins\?username\=ugtheven').subscribe(data => {
+      this.nbWins = data.wins.length;
+      for (let i = 0; i < this.nbWins; i++){
+        this.gameHistory.push(data.wins[i])
+      }
+    })
+    //LOSES
+    this.http.get<LoseHistory>('http://localhost:3000/game/loses\?username\=ugtheven').subscribe(data => {
+      this.nbLoses = data.loses.length;
+      for (let i = 0; i < this.nbLoses; i++){
+        this.gameHistory.push(data.loses[i])
+      }
+    })
+    this.nbGames = this.gameHistory.length;
+    //OUTPUT
+    console.log('Nombre de parties: ', this.nbGames);
+    console.log('Nombre de victoires: ', this.nbWins);
+    console.log('Nombre de defaites: ', this.nbLoses);
+    console.log(this.gameHistory);
   }
 }
