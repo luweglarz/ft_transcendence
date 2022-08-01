364a2ffe2907c4e9c120851e21e976fae3b3535a
mjacq <mjacq@redoute.fr>
2022-08-01 11:39:05 +0200

    signup: front username availability validator

---
+++ src/frontend/src/app/auth/signup/validators/validator-builder.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  ValidationErrors,
+} from '@angular/forms';
+import { map, tap, Observable, take, catchError, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ValidatorBuilderService {
+  constructor(private http: HttpClient) {}
+
+  isAvailable(field: string): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      const check$ = this.http
+        .get<{ exists: boolean }>(
+          `${environment.backend}/auth/exists/${field}/${control.value}`,
+        )
+        .pipe(
+          catchError((err) => {
+            console.error(err);
+            return of(null);
+          }),
+          take(1),
+          tap((value) => console.log(value)),
+          map(
+            (value) =>
+              value?.exists ? { fieldAvailable: 'unavailable' } : null,
+            // note: can't use {fieldAvailable: false},
+            // because we want to test `if (username.errors.fieldAvailable)
+            // and *ngIf="'fieldAvailable' in username.errors" works but unfortunately logs an error
+          ),
+        );
+      return check$;
+    };
+  }
+}
