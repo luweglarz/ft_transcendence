a967b2092ca3cc15ba3a0e7ad748a6164e41047d
ugtheven <ugotheveny@student.42.fr>
2022-08-01 19:17:03 +0200

    Finally fixed, now using promise and async functions #68

--- src/frontend/src/app/home-page/popups/profil/match-history/match-history.service.ts
+++ src/frontend/src/app/home-page/popups/profil/match-history/match-history.service.ts
@@ -2,20 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { JwtService } from 'src/app/auth/jwt';
 
- interface Game {
-   id: number;
-   createdAt: string;
-   winnerId: number;
-   winnerGoals: number;
-   loserId: number;
-   loserGoals: number;
- }
+interface Game {
+  id: number;
+  createdAt: string;
+  winnerId: number;
+  winnerGoals: number;
+  loserId: number;
+  loserGoals: number;
+}
 
- interface WinHistory {
-    wins: Array<Game>;
- }
+interface WinHistory {
+  wins: Array<Game>;
+}
 
- interface LoseHistory {
+interface LoseHistory {
   loses: Array<Game>;
 }
 
@@ -34,122 +34,116 @@ export class MatchHistoryService {
   }
 
   //UTILS
-  sortHistory() {
-    this.gameHistory.sort((n1, n2) => {
-      if (n1.id > n2.id) {
-        return 1;
-      }
-      if (n1.id < n2.id) {
-        return -1;
-      }
-      return 0;
-    })
-  }
+  // sortHistory() {
+  //   this.gameHistory.sort((n1, n2) => {
+  //     if (n1.id > n2.id) {
+  //       return 1;
+  //     }
+  //     if (n1.id < n2.id) {
+  //       return -1;
+  //     }
+  //     return 0;
+  //   })
+  // }
+
+  //TEST
+
 
   //RETRIEVE WIN LOSES COUNTER
-  retrieveWinCounter(username: string): number{
-    let nbWins: number = -1;
-    this.http.get<WinHistory>('http://localhost:3000/game/wins\?username\=' + username).subscribe(data => {
-      nbWins = data.wins.length;
-      return (nbWins);
-    })
-    return (nbWins);
+  async retrieveWinCounter(username: string){
+    return new Promise(resolve => {
+      this.http.get<WinHistory>('http://localhost:3000/game/wins\?username\=' + username).subscribe(data => {
+        resolve (data.wins.length);
+      })
+    });
   }
 
-  retrieveLoseCounter(username: string): number{
-    let nbLoses: number = -1;
-    this.http.get<LoseHistory>('http://localhost:3000/game/loses\?username\=' + username).subscribe(data => {
-      nbLoses = data.loses.length;
-      return (nbLoses);
-    })
-    return (nbLoses);
+  async retrieveLoseCounter(username: string){
+    return new Promise(resolve => {
+      this.http.get<LoseHistory>('http://localhost:3000/game/loses\?username\=' + username).subscribe(data => {
+        resolve (data.loses.length);
+      })
+    });
   }
 
   //RETRIEVE USER HISTORY
-  retrieveWonGames(username: string): Array<Game> {
+  async retrieveWonGames(username: string): Promise<Array<Game>> {
     let winHistory: Array<Game> = [];
-    this.http.get<WinHistory>('http://localhost:3000/game/wins\?username\=' + username).subscribe(data => {
-      for (let i = 0; i < data.wins.length; i++){
-        winHistory.push(data.wins[i])
-      }
-      return (winHistory);
-    })
-    return (winHistory);
+    return new Promise(resolve => {
+      this.http.get<WinHistory>('http://localhost:3000/game/wins\?username\=' + username).subscribe(data => {
+        data.wins.forEach(val => winHistory.push(Object.assign({}, val)));
+        resolve (winHistory);
+      })
+    });
   }
 
-  retrieveLostGames(username: string): Array<Game> {
+  async retrieveLostGames(username: string): Promise<Array<Game>> {
     let loseHistory: Array<Game> = [];
-    this.http.get<LoseHistory>('http://localhost:3000/game/loses\?username\=' + username).subscribe(data => {
-      for (let i = 0; i < data.loses.length; i++){
-        loseHistory.push(data.loses[i])
-      }
-      return (loseHistory);
-    })
-    return (loseHistory);
+    return new Promise(resolve => {
+      this.http.get<LoseHistory>('http://localhost:3000/game/loses\?username\=' + username).subscribe(data => {
+        data.loses.forEach(val => loseHistory.push(Object.assign({}, val)));
+        resolve (loseHistory);
+      })
+    });
   }
 
-  retrieveUserHistory(username: string): Array<Game> {
-    let winHistory: Array<Game> = this.retrieveWonGames(username);
-    let loseHistory: Array<Game> = this.retrieveLostGames(username);
-    let gameHistory: Array<Game> = [...winHistory, ...loseHistory];
-    //this.sortHistory();
+  async retrieveUserHistory(username: string): Promise<Array<Game>> {
+    let winHistory: Array<Game> = await this.retrieveWonGames(username);
+    let loseHistory: Array<Game> = await this.retrieveLostGames(username);
+    let gameHistory: Array<Game> = winHistory.concat(loseHistory);
     return (gameHistory);
   }
 
+
   //UPDATE HISTORY
-  updateWinHistory(username: string) {
-    this.http.get<WinHistory>('http://localhost:3000/game/wins\?username\=' + username).subscribe(data => {
-      var tmp: Array<Game> = [];
-      for (let i = 0; i < data.wins.length; i++){
-        tmp.push(data.wins[i])
-      }
-      tmp = tmp.filter((game) => game.id > this.nbGames);
-      this.nbGames += tmp.length;
-      for (let i = 0; i < tmp.length; i++){
-        this.gameHistory.push(tmp[i])
-      }
-    }, error => {
-      console.log('ERROR: getting wins');
-    })
-  }
+  // updateWinHistory(username: string) {
+  //   this.http.get<WinHistory>('http://localhost:3000/game/wins\?username\=' + username).subscribe(data => {
+  //     var tmp: Array<Game> = [];
+  //     for (let i = 0; i < data.wins.length; i++){
+  //       tmp.push(data.wins[i])
+  //     }
+  //     tmp = tmp.filter((game) => game.id > this.nbGames);
+  //     this.nbGames += tmp.length;
+  //     for (let i = 0; i < tmp.length; i++){
+  //       this.gameHistory.push(tmp[i])
+  //     }
+  //   }, error => {
+  //     console.log('ERROR: getting wins');
+  //   })
+  // }
 
-  updateLoseHistory(username: string) {
-    this.http.get<LoseHistory>('http://localhost:3000/game/loses\?username\=' + username).subscribe(data => {
-      var tmp: Array<Game> = [];
-      for (let i = 0; i < data.loses.length; i++){
-        tmp.push(data.loses[i])
-      }
-      tmp = tmp.filter((game) => game.id > this.nbGames);
-      this.nbGames += tmp.length;
-      for (let i = 0; i < tmp.length; i++){
-        this.gameHistory.push(tmp[i])
-      }
-    }, error => {
-      console.log('ERROR: getting wins');
-    })
-  }
+  // updateLoseHistory(username: string) {
+  //   this.http.get<LoseHistory>('http://localhost:3000/game/loses\?username\=' + username).subscribe(data => {
+  //     var tmp: Array<Game> = [];
+  //     for (let i = 0; i < data.loses.length; i++){
+  //       tmp.push(data.loses[i])
+  //     }
+  //     tmp = tmp.filter((game) => game.id > this.nbGames);
+  //     this.nbGames += tmp.length;
+  //     for (let i = 0; i < tmp.length; i++){
+  //       this.gameHistory.push(tmp[i])
+  //     }
+  //   }, error => {
+  //     console.log('ERROR: getting wins');
+  //   })
+  // }
 
-
-  updateUserHistory(username: string) {
-    this.updateWinHistory(username);
-    this.updateLoseHistory(username);
-    this.sortHistory();
-  }
-
-
+  // updateUserHistory(username: string) {
+  //   this.updateWinHistory(username);
+  //   this.updateLoseHistory(username);
+  //   //this.sortHistory();
+  // }
 
   //DEBUG
   testButton() {
     console.log('Username: ', this.jwtService.getPayload()?.username);
-    console.log('Nombre de parties: ', this.nbGames);
-    //console.log('Nombre de victoires: ', this.nbWins);
-    //console.log('Nombre de defaites: ', this.nbLoses);
+
     console.log('Nombre de victoires: ', this.retrieveWinCounter('ugtheven'));
     console.log('Nombre de defaites: ', this.retrieveLoseCounter('ugtheven'));
-    //this.updateUserHistory('ugtheven');
+
     console.log('Win History', this.retrieveWonGames('ugtheven'));
-    console.log('Loss History', this.retrieveLostGames('ugtheven'));
-    //console.log('Game History', this.gameHistory);
+    console.log('Lose History', this.retrieveLostGames('ugtheven'));
     console.log('Game History', this.retrieveUserHistory('ugtheven'));
+    //let gameHistory: Array<Game> = this.retrieveUserHistory('ugtheven');
   }
 }
