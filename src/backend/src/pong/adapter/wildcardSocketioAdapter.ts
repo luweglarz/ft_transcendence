24819ec04f2fa5b8488e46da9240fdb00f900952
RolandGamos <lucasweglarz9@gmail.com>
2022-08-02 15:15:10 +0200

    Import wildcard fix

--- src/backend/src/pong/adapter/wildcardSocketioAdapter.ts
+++ src/backend/src/pong/adapter/wildcardSocketioAdapter.ts
@@ -1,12 +1,12 @@
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { EventEmitter } from 'events';
 import { Server } from 'socket.io';
-import wildcard from 'socketio-wildcard';
 
+const sioWildcard = require('socketio-wildcard');
 export default class WildcardsIoAdapter extends IoAdapter {
   createIOServer(port: number, options?: any): any {
     const server: Server = super.createIOServer(port, options);
-    server.use(wildcard(EventEmitter));
+    server.use(sioWildcard(EventEmitter));
     return server;
   }
 }
