d72c6731a70588af357165cd7487c8b13fcd1b15
mjacq <mjacq@redoute.fr>
2022-08-03 14:11:39 +0200

    chat: group front components in a module

--- src/frontend/src/app/chat/chat-room-create/chat-room-create.component.ts
+++ src/frontend/src/app/chat/chat-room-create/chat-room-create.component.ts
@@ -1,10 +1,4 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Room } from 'src/app/interface/room';
 import { RoomType } from 'src/app/interface/room';
