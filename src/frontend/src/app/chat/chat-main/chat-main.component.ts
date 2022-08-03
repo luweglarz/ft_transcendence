fd3c2aac79dbda5aec9bd85b0c78a6aa05099182
mjacq <mjacq@redoute.fr>
2022-08-03 17:15:12 +0200

    chat: mv folders class, interface and service/chatService into the chat module

--- src/frontend/src/app/chat/chat-main/chat-main.component.ts
+++ src/frontend/src/app/chat/chat-main/chat-main.component.ts
@@ -2,8 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSelectionListChange } from '@angular/material/list';
 import { Observable } from 'rxjs';
-import { Room } from 'src/app/interface/room';
-import { ChatService } from 'src/app/services/chatService/chat.service';
+import { Room } from 'src/app/chat/interface/room';
+import { ChatService } from 'src/app/chat/chatService/chat.service';
 import { ChatRoomCreateComponent } from '../chat-room-create/chat-room-create.component';
 
 @Component({
