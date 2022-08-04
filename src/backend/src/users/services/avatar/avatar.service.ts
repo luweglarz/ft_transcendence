171c4e6c110432b3984b714e42cb82166fd0c166
mjacq <mjacq@redoute.fr>
2022-08-04 09:57:04 +0200

    avatar: backend refact (cleaner code)

--- src/backend/src/users/services/avatar/avatar.service.ts
+++ src/backend/src/users/services/avatar/avatar.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  StreamableFile,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { JwtPayload } from 'src/auth/interfaces';
 import { DbService } from 'src/db/db.service';
 
@@ -19,15 +24,21 @@ export class AvatarService {
     }
   }
 
-  async getAvatar(username: string) {
-    try {
-      const avatar = await this.db.avatar.findFirst({
-        where: { user: { username: username } },
-      });
-      return avatar;
-    } catch (err) {
-      console.log(`Error: ${this.uploadAvatar.name} failed.`);
-      throw new InternalServerErrorException('Could not download the avatar');
-    }
+  async getResponse(username: string, res: Response) {
+    const avatar = await this.findAvatar(username);
+    if (avatar) return this.fileResponse(avatar.image, avatar.mimeType, res);
+    else return '';
+  }
+
+  async findAvatar(username: string) {
+    const avatar = await this.db.avatar.findFirst({
+      where: { user: { username: username } },
+    });
+    return avatar;
+  }
+
+  fileResponse(file: Buffer, contentType: string, res: Response) {
+    res.type(contentType);
+    return new StreamableFile(file);
   }
 }
