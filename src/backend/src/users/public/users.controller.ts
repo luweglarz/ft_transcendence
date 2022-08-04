06fc290ce888a8143490f8572887d0ce8c9edea1
mjacq <mjacq@redoute.fr>
2022-08-04 12:08:37 +0200

    avatar: backend route to check if user has avatar

--- src/backend/src/users/public/users.controller.ts
+++ src/backend/src/users/public/users.controller.ts
@@ -1,17 +1,22 @@
 import { Controller, Get, Param, Res, UseGuards } from '@nestjs/common';
-import { JwtGuard } from 'src/auth/guard';
+// import { JwtGuard } from 'src/auth/guard';
 import { Response } from 'express';
 import { AvatarService } from '../services/avatar/avatar.service';
 import { UsersService } from '../services/users/users.service';
 
 @Controller('users')
-@UseGuards(JwtGuard)
+// @UseGuards(JwtGuard)
 export class UsersController {
   constructor(private service: UsersService, private avatar: AvatarService) {}
 
   @Get('')
   async listUsers() {
     return this.service.listUsers();
+  }
+
+  @Get(':username/has-avatar')
+  async hasAvatar(@Param('username') username: string) {
+    return this.avatar.hasAvatar(username);
   }
 
   @Get(':username/avatar')
