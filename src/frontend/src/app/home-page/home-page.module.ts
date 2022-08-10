9075f67606a7f7f9663018a55b080259bc842c38
ugtheven <ugotheveny@student.42.fr>
2022-08-10 20:12:56 +0200

    Popups now have his module

--- src/frontend/src/app/home-page/home-page.module.ts
+++ src/frontend/src/app/home-page/home-page.module.ts
@@ -3,32 +3,30 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ChatbarComponent } from './chatbar/chatbar.component';
 import { HomeComponent } from './home/home.component';
-import { PopupsComponent } from './popups/popups.component';
 import { CollapseService } from './collapse.service';
 import { NotificationService } from './notification.service';
 import { PopupsService } from './popups/popups.service';
 import { GameService } from '../pong/game/game.service';
 import { PongModule } from '../pong/pong.module';
-import { ProfilComponent } from './popups/profil/profil.component';
 import { LadderComponent } from './popups/ladder/ladder.component';
 import { SocialComponent } from './popups/social/social.component';
 import { RouterModule } from '@angular/router';
-import { MatchHistoryComponent } from './popups/profil/match-history/match-history.component';
 import { ChatModule } from '../chat/chat.module';
 import { AvatarModule } from '../avatar';
+import { ProfilInfoService } from './popups/profil/profil-info.service';
+import { PopupsModule } from './popups/popups.module';
+import { PopupsComponent } from './popups/popups.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     NavbarComponent,
     ChatbarComponent,
-    PopupsComponent,
-    ProfilComponent,
-    LadderComponent,
-    SocialComponent,
-    MatchHistoryComponent,
+    PopupsComponent
   ],
-  providers: [CollapseService, PopupsService, NotificationService, GameService],
-  imports: [CommonModule, PongModule, RouterModule, ChatModule, AvatarModule],
+  providers: [CollapseService, PopupsService, NotificationService, GameService, ProfilInfoService],
+  imports: [CommonModule, PongModule, RouterModule, ChatModule, AvatarModule, PopupsModule],
 })
-export class HomePageModule {}
+export class HomePageModule {
+  //
+}
