7920a036350fd73b760d1373c889c2c3193841ad
ugtheven <ugotheveny@student.42.fr>
2022-08-10 12:34:43 +0200

    Some enhancements (animations, other modes...) on matchmaking page, can be stylized more

--- src/frontend/src/app/pong/matchmaking/matchmaking.component.ts
+++ src/frontend/src/app/pong/matchmaking/matchmaking.component.ts
@@ -8,45 +8,33 @@ import { MatchmakingService } from './matchmaking.service';
   styleUrls: ['./matchmaking.component.css'],
 })
 export class MatchmakingComponent {
-  //Original gamemode
+  //Queues
   originalQueue = false;
-  nbPlayersOriginal = 0;
-  //timer
-  msInQueue = 0;
-  secInQueue = 0;
-  running = false;
-  timer = 0;
+  funQueue = false;
+  rankedQueue = false;
 
   constructor(
     private matchmakingService: MatchmakingService,
     public collapseService: CollapseService,
   ) {}
 
-  startTimer(queueBool: boolean) {
-    if (queueBool === true) {
-      const startTime = Date.now() - (this.msInQueue || 0);
-      this.timer = setInterval(() => {
-        this.msInQueue = Date.now() - startTime;
-        if (this.msInQueue / 1000 >= this.secInQueue) this.secInQueue++;
-      });
-    } else clearInterval(this.timer);
-  }
-
-  clearTimer() {
-    this.msInQueue = 0;
-    this.secInQueue = 0;
-    clearInterval(this.timer);
-  }
-
   buttonRequestJoinNormalMatchMaking() {
     this.matchmakingService.requestJoinNormalMatchMaking();
     this.originalQueue = true;
-    this.startTimer(this.originalQueue);
+  }
+
+  buttonRequestJoinFunMatchMaking() {
+    this.funQueue = true;
+  }
+
+  buttonRequestJoinRankedMatchMaking() {
+    this.rankedQueue = true;
   }
 
   buttonRequestLeaveMatchMaking() {
     this.matchmakingService.requestLeaveMatchMaking();
     this.originalQueue = false;
-    this.clearTimer();
+    this.funQueue = false;
+    this.rankedQueue = false;
   }
 }
