5c1c61220863e16b6f202175a607c35deab208d9
ugtheven <ugotheveny@student.42.fr>
2022-08-10 18:09:58 +0200

    Need to refactorize the structure of popups to fix the lazyloading issue

--- src/frontend/src/app/home-page/popups/profil/match-history/match-history.component.ts
+++ src/frontend/src/app/home-page/popups/profil/match-history/match-history.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { JwtService } from 'src/app/auth/jwt';
-
 import { AvatarService } from 'src/app/avatar/avatar.service';
 import { ProfilInfoService } from '../profil-info.service';
 
@@ -15,9 +14,11 @@ interface User {
   templateUrl: './match-history.component.html',
   styleUrls: ['./match-history.component.css'],
 })
-export class MatchHistoryComponent implements OnInit {
-  users: Array<User> = [];
-  username = '';
+export class MatchHistoryComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    console.log('Jai fini de load');
+  }
 
   constructor(
     public profilInfoService: ProfilInfoService,
@@ -25,18 +26,6 @@ export class MatchHistoryComponent implements OnInit {
     private http: HttpClient,
     private jwtService: JwtService,
   ) {
-    this.http
-      .get<Array<string>>('http://localhost:3000/users/')
-      .subscribe((data) => {
-        for (let i = 0; i < data.length; i++) {
-          this.users.push({ username: data[i], id: i - 1 });
-        }
-      });
-    const tmp = this.jwtService.getPayload()?.username;
-    if (tmp != undefined) this.username = tmp;
-  }
-
-  ngOnInit(): void {
     //
   }
 }
