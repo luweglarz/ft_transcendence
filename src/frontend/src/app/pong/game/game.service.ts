ccc1893bdae98fbbfeca77f7eb792b23c6116ea3
RolandGamos <lucasweglarz9@gmail.com>
2022-08-10 16:04:29 +0200

    Improve game-custom frontend #62

--- src/frontend/src/app/pong/game/game.service.ts
+++ src/frontend/src/app/pong/game/game.service.ts
@@ -123,16 +123,11 @@ export class GameService {
       boostTwo.nativeElement.width,
       boostTwo.nativeElement.height,
     );
-    console.log(
-      'native eleenmt width height : ',
-      boostOne.nativeElement.width + ' , ',
-      boostOne.nativeElement.height,
-    );
     this.boostOneContext.fillStyle = 'red';
     this.boostTwoContext.fillStyle = 'red';
     const boostOneRatio = Math.abs((players[0].boostCd / 5000) * 100 - 100);
-    this.boostOneContext.fillRect(0, 0, (boostOneRatio * 300) / 100, 150);
+    this.boostOneContext.fillRect(0, 0, (boostOneRatio * 300) / 100, boostOne.nativeElement.height);
     const boostTwoRatio = Math.abs((players[1].boostCd / 5000) * 100 - 100);
-    this.boostTwoContext.fillRect(0, 0, (boostTwoRatio * 300) / 100, 150);
+    this.boostTwoContext.fillRect(0, 0, (boostTwoRatio * 300) / 100, boostTwo.nativeElement.height);
   }
 }
