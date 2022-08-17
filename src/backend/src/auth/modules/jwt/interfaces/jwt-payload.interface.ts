61fad98c2124af21ba73186526c34dbb4cdb6641
mjacq <mjacq@redoute.fr>
2022-08-17 19:56:56 +0200

    auth: some refactoring

--- src/backend/src/auth/modules/jwt/interfaces/jwt-payload.interface.ts
+++ src/backend/src/auth/modules/jwt/interfaces/jwt-payload.interface.ts
@@ -1,10 +1,4 @@
-import { OAuthUserDto } from '../../oauth/dto';
-
 export interface JwtPayload {
   sub: number; // user id
   username: string;
-}
-
-export interface OAuthJwtPayload {
-  oAuthUser: OAuthUserDto;
 }
--- src/frontend/src/app/auth/interface/auth.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface OAuthUser {
-  login: string;
-  email: string;
-  image_url: string;
-}
-
-export interface OAuthJwtPayload {
-  oAuthUser: OAuthUser;
-}
