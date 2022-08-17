70523c02fee5ef4f122763cb6918dc59ddfd797a
ugtheven <ugotheveny@student.42.fr>
2022-08-18 01:08:49 +0200

    HTML/CSS structure of the friend/block list #97

--- src/frontend/src/app/home-page/popups/social/social.component.ts
+++ src/frontend/src/app/home-page/popups/social/social.component.ts
@@ -1,11 +1,97 @@
 import { Component, OnInit } from '@angular/core';
 
+interface User{
+  username: string;
+  friend: boolean;
+  blocked: boolean;
+  online: boolean;
+}
+
 @Component({
   selector: 'app-social',
   templateUrl: './social.component.html',
   styleUrls: ['./social.component.css'],
 })
 export class SocialComponent implements OnInit {
+
+  users :Array<User> = [{
+    username:'ugtheven',
+    friend:true,
+    blocked:false,
+    online:true,
+  },{
+    username:'usertest',
+    friend:true,
+    blocked:false,
+    online:true,
+  },{
+    username:'usertest',
+    friend:true,
+    blocked:false,
+    online:false,
+  },{
+    username:'usertest',
+    friend:true,
+    blocked:false,
+    online:true,
+  },{
+    username:'usertest',
+    friend:true,
+    blocked:false,
+    online:false,
+  },{
+    username:'usertest',
+    friend:true,
+    blocked:false,
+    online:false,
+  },{
+    username:'usertest',
+    friend:true,
+    blocked:false,
+    online:true,
+  },{
+    username:'usertest',
+    friend:true,
+    blocked:false,
+    online:false,
+  },{
+    username:'usertest',
+    friend:true,
+    blocked:false,
+    online:true,
+  },{
+    username:'usertest',
+    friend:true,
+    blocked:false,
+    online:false,
+  },{
+    username:'usertest',
+    friend:true,
+    blocked:false,
+    online:true,
+  },{
+    username:'usertest',
+    friend:true,
+    blocked:false,
+    online:false,
+  },{
+    username:'usertest',
+    friend:true,
+    blocked:false,
+    online:false,
+  },{
+    username:'usertest',
+    friend:true,
+    blocked:false,
+    online:true,
+  },{
+    username:'usertest',
+    friend:true,
+    blocked:false,
+    online:false,
+  },];
+  showFriends: boolean = true;
+
   constructor() {
     //
   }
@@ -13,4 +99,13 @@ export class SocialComponent implements OnInit {
   ngOnInit(): void {
     //
   }
+
+  selectFriends() {
+    this.showFriends = true;
+  }
+
+  selectBlocked() {
+    this.showFriends = false;
+  }
+
 }
