5e7c961599d21344bb69c262938e833e3f0f04e8
mjacq <mjacq@redoute.fr>
2022-08-17 17:19:36 +0200

    jwt: backend methods to manipulate refresh tokens

--- src/backend/src/auth/modules/jwt/jwt-auth.service.ts
+++ src/backend/src/auth/modules/jwt/jwt-auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { DbService } from 'src/db/db.service';
 import { JwtPayload, OAuthJwtPayload } from './interfaces';
 
 @Injectable()
@@ -8,16 +9,27 @@ export class JwtAuthService {
   private readonly _refreshTokenSecret = `refresh ${process.env['JWT_SECRET']}`;
   private readonly _tempTokenSecret = `temp ${process.env['JWT_SECRET']}`;
 
-  constructor(private jwt: JwtService) {}
+  constructor(private jwt: JwtService, private db: DbService) {}
+
+  async tokens(payload: JwtPayload) {
+    const tokenList = await Promise.all([
+      this.signAccessToken(payload),
+      this.signRefreshToken(payload),
+    ]);
+    const tokens = { access: tokenList[0], refresh: tokenList[1] };
+    this.saveRefreshToken(payload.sub, tokens.refresh);
+    return tokens;
+  }
 
   //  =============================== Getters ================================  //
+
+  // Secrets are required to configure the strategies
 
   get accessTokenSecret() {
     return this._accessTokenSecret;
   }
-
   get refreshTokenSecret() {
-    return this._accessTokenSecret;
+    return this._refreshTokenSecret;
   }
 
   //  ============================== Sign token ==============================  //
@@ -28,14 +40,12 @@ export class JwtAuthService {
       secret: this._accessTokenSecret,
     });
   }
-
   signRefreshToken(payload: JwtPayload): Promise<string> {
     return this.jwt.signAsync(payload, {
-      expiresIn: '42m',
+      expiresIn: '42h',
       secret: this._refreshTokenSecret,
     });
   }
-
   signTempToken(payload: OAuthJwtPayload): Promise<string> {
     return this.jwt.signAsync(payload, {
       expiresIn: '10m',
@@ -56,6 +66,35 @@ export class JwtAuthService {
   }
   verifyTempToken(token: string) {
     return this.jwt.verifyAsync(token, { secret: this._tempTokenSecret });
+  }
+
+  //  ================= Save / Clear / Compare refresh token =================  //
+
+  // @brief store token in the database
+  private saveRefreshToken(userId: number, rt: string) {
+    this.db.auth.update({
+      data: { refreshToken: rt },
+      where: { userId: userId },
+    });
+  }
+
+  // @brief remove refresh token stored in db for a given user
+  // Needed when sign out
+  clearRefreshToken(userId: number) {
+    this.db.auth.update({
+      data: { refreshToken: null },
+      where: { userId: userId },
+    });
+  }
+
+  // @brief return true if given refresh token matches the on in the database
+  async compareRefreshToken(userId: number, rt: string) {
+    if (!rt) return false;
+    const { refreshToken } = await this.db.auth.findFirst({
+      select: { refreshToken: true },
+      where: { userId: userId },
+    });
+    return refreshToken === rt;
   }
 
   //  ================================ Other =================================  //
