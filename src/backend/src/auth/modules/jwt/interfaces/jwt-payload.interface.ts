9b08ed5d31d10284f949383a1b49be61907cdcb1
mjacq <mjacq@redoute.fr>
2022-08-18 12:06:51 +0200

    auth: backend route to refresh tokens

--- src/backend/src/auth/modules/jwt/interfaces/jwt-payload.interface.ts
+++ src/backend/src/auth/modules/jwt/interfaces/jwt-payload.interface.ts
@@ -2,3 +2,8 @@ export interface JwtPayload {
   sub: number; // user id
   username: string;
 }
+
+export interface JwtData extends JwtPayload {
+  iat: number;
+  exp: number;
+}
--- src/backend/src/auth/modules/jwt/jwt-auth.controller.ts
+++ src/backend/src/auth/modules/jwt/jwt-auth.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from 'src/auth/decorators';
+import { JwtRefreshGuard } from './guard';
+import { JwtPayload } from './interfaces';
 import { JwtAuthService } from './jwt-auth.service';
 
 @Controller()
 export class JwtAuthController {
   constructor(private service: JwtAuthService) {}
 
-  @Post('refresh')
-  refreshToken() {
-    return this.service.refreshToken();
+  @Get('refresh')
+  @UseGuards(JwtRefreshGuard)
+  refreshToken(@User() user: JwtPayload) {
+    return this.service.newTokens(user);
   }
 }
