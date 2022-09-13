1df28a071b18502743cc8537830f1b33a37f0478
ugtheven <ugotheveny@student.42.fr>
2022-09-13 18:04:17 +0200

    Implementation of from/to for whispers #118

--- src/frontend/src/app/chat/components/chat-message/chat-message.component.ts
+++ src/frontend/src/app/chat/components/chat-message/chat-message.component.ts
@@ -1,5 +1,6 @@
 import { Message } from 'src/app/chat/interface/message';
 import { Component, Input } from '@angular/core';
+import { JwtService } from 'src/app/auth/jwt';
 
 @Component({
   selector: 'app-chat-message',
@@ -8,7 +9,13 @@ import { Component, Input } from '@angular/core';
 })
 export class ChatMessageComponent /*implements OnInit*/ {
   @Input() message: Message = {};
-  //constructor() {}
+  username: string = '';
+
+  constructor(private jwtService: JwtService) {
+    const username = this.jwtService.username;
+    if (username) this.username = username;
+  }
+
   //ngOnInit(): void {}
   //findUser(message: Message): RoomUser {
   //  this.roomUsers
