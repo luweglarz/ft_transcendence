0e25c1d9bad85bf3144a89b048b462acb392664d
mjacq <>
2022-09-15 19:48:28 +0200

    Avatar upload in settings: display current avatar on init

--- src/frontend/src/app/home-page/popups/settings/settings.component.ts
+++ src/frontend/src/app/home-page/popups/settings/settings.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { JwtService } from 'src/app/auth/jwt';
 import { AvatarUploadService } from 'src/app/avatar/avatar-upload/avatar-upload.service';
+import { AvatarService } from 'src/app/avatar/avatar.service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css'],
 })
-export class SettingsComponent implements OnInit {
-  constructor(public avatar: AvatarUploadService) {}
+export class SettingsComponent implements OnInit, OnDestroy {
+  constructor(
+    public avatarUpload: AvatarUploadService,
+    public avatar: AvatarService,
+    private jwt: JwtService,
+  ) {}
 
   ngOnInit(): void {
-    //
+    this.avatar.getSrc(this.jwt.username).subscribe((src) => {
+      this.avatarUpload.update({ src: src });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.avatarUpload.clear();
   }
 }
